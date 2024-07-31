c7c873f3eb1f48510b7fc584fd85ead69c64a9d4
mayureshambatwar <160317267+mayureshambatwar@users.noreply.github.com>
2024-07-31 13:21:10 +0530

    new

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,6 +1,13 @@
 import React from "react";
 import "./footer.css";
-import { FaFacebook, FaGithub, FaHome, FaInstagram, FaMailBulk, FaPhone,  } from "react-icons/fa";
+import {
+  FaFacebook,
+  FaGithub,
+  FaHome,
+  FaInstagram,
+  FaMailBulk,
+  FaPhone,
+} from "react-icons/fa";
 
 const Footer = () => {
   return (
@@ -15,37 +22,66 @@ const Footer = () => {
             </div>
           </div>
           <div className="phone">
-            
             <h4>
-              {" "}
               <FaPhone
                 size={20}
                 style={{ color: "#fff", marginRight: "2rem" }}
               />
-              1-2323-124-23
+              92846-310-83
             </h4>
           </div>
           <div className="email">
             <h4>
-              {" "}
               <FaMailBulk
                 size={20}
                 style={{ color: "#fff", marginRight: "2rem" }}
               />
-              info@gamil.com
+              <a
+                href="mailto:mayureshambatwar@gmail.com"
+                className="mail"
+              >
+                mayureshambatwar@gmail.com
+              </a>
             </h4>
           </div>
         </div>
         <div className="right">
           <h4>About this Company</h4>
           <p>
-            This is me Mayuresh.CEO & Founder of Teck.I enjoy discussing new
-            project and desing challenges
+            This is me Mayuresh. CEO & Founder. I enjoy discussing new
+            projects and design challenges.
           </p>
           <div className="social">
-            <FaFacebook size={30} style={{ color: "#fff", marginRight: "1rem" }} />
-            <FaInstagram size={30} style={{ color: "#fff", marginRight: "1rem" }} />
-            <FaGithub size={30} style={{ color: "#fff", marginRight: "1rem" }} />
+            <a
+              href="https://www.facebook.com/mayuresh.ambatwar"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaFacebook
+                size={30}
+                style={{ color: "#fff", marginRight: "1rem" }}
+              />
+            </a>
+            <a
+              href="https://www.instagram.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaInstagram
+                size={30}
+                style={{ color: "#fff", marginRight: "1rem" }}
+              />
+            </a>
+            <a
+              href="https://github.com/mayureshambatwar"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaGithub
+                size={30}
+                style={{ color: "#fff", marginRight: "1rem" }}
+              />
+            </a>
           </div>
         </div>
       </div>
