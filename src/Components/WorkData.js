99c810eeaa0b1f1eaf6ad102b28b07371891741e
mayureshambatwar <160317267+mayureshambatwar@users.noreply.github.com>
2025-01-10 11:59:18 +0530

    Updated home page with new banner design

--- src/Components/WorkData.js
+++ src/Components/WorkData.js
@@ -2,35 +2,43 @@ import Pro1 from "../assets/project1.png";
 import Pro2 from "../assets/project2.png";
 import Pro3 from "../assets/project3.png";
 import Pro4 from "../assets/project5.png"
+import Pro5 from "../assets/project6.png"
 
 const projectData = [
   {
     imgsrc: Pro1,
     title: "Ecommerce Website Design",
-    text: "The Ecommerce Website Design project focuses on creating a modern, user-friendly online shopping platform that enhances the customer experience. It features a sleek, responsive design that works seamlessly across all devices, allowing users to browse products, filter by categories, and make purchases with ease. The design emphasizes intuitive navigation, fast load times, and secure checkout processes to ensure a smooth and enjoyable shopping experience for all users.",
+    text: "The E-commerce Website is a modern, user-friendly online shopping platform built with React.js and styled using Tailwind CSS , designed to provide a seamless and engaging shopping experience. The home page highlights featured products, categories, and promotional offers with a visually appealing hero banner showcasing sales or trending items. The product catalog displays products in a grid or list format with images, names, prices, and ratings, allowing users to filter and sort by categories, price, or popularity. Each product has a detailed page with images, descriptions, specifications, and customer reviews, along with an 'Add to Cart' button and availability status. The shopping cart enables users to view added items, update quantities, and proceed to checkout, with a summary of the order displayed. The checkout process supports guest checkout for convenience and offers a smooth flow for completing purchases",
     view:"https://e-commerce-web-pearl.vercel.app/",
     source:"https://github.com/mayureshambatwar/E-commerce-Web"
   },
   {
     imgsrc: Pro2,
     title: "Power BI",
-    text: "Welcome, Students Are you ready to unlock the secrets of data analysis and visualization? Look no further! Our Power BI course is designed specifically for students like you, to help you master the skills needed to succeed in today's data-driven world.",
+    text: "The Power BI Landing Page is a responsive and visually captivating web page designed to showcase the features, benefits, and use cases of Power BI, utilizing modern web technologies such as HTML, CSS, JavaScript, and frameworks like React.js for dynamic content and interactivity. The hero section features a striking banner with a compelling headline, high-quality visuals of Power BI dashboards, and a prominent call-to-action button to immediately engage users. Organized in a visually appealing grid or carousel layout, the page highlights Power BI's key features, including real-time analytics, interactive dashboards, seamless data integration, and AI-powered insights. It presents real-world use cases across industries like finance, healthcare, retail, and education, supported by illustrative charts and scenarios",
     view:"https://power-bi-gamma.vercel.app/",
     source:"https://github.com/mayureshambatwar/Power-Bi"
   },
   {
     imgsrc: Pro3,
     title: "Dashboard",
-    text: "The dashboard displays key metrics using graphs, charts, and tables. This could include things like sales data, system performance, user activity, or project progress. Dashboards are designed to be accessible across different devices (desktops, tablets, mobile phones), ensuring that the user experience is optimized for each",
+    text: "The Dashboard App is a dynamic web application built with React.js and styled with Tailwind CSS, designed to provide a centralized platform for tracking and managing essential metrics through data visualization and interaction. It integrates charts, graphs, and tables, including bar charts, pie charts, and line graphs, to present data in a structured and intuitive format, ensuring real-time or periodically updated accuracy by seamlessly fetching data from APIs or databases. Dashboards are designed to be accessible across different devices (desktops, tablets, mobile phones), ensuring that the user experience is optimized for each",
     view:"https://dashboard-main-murex.vercel.app/",
     source:"https://github.com/mayureshambatwar/Dashboard"
   },
   {
     imgsrc: Pro4,
     title: "Countries-API-Project",
-    text: "The Countries API Project is a web application that allows users to explore information about countries worldwide. By leveraging the REST Countries API, users can search for countries, filter them by region, and view detailed information about each country, including population, region, capital, and languages. The app also includes a dark mode toggle for a customizable user experience.",
+    text: "The Countries API Project is a web application built using React.js and styled with Tailwind CSS, leveraging the REST Countries API to provide users with an interactive platform to explore global data. The app displays a grid or list view of countries, showcasing flags, names, and basic details such as population, region, and capital, allowing users to visually browse countries. It features a search bar for finding countries by name or keyword with real-time filtering and a dropdown menu for region-based filtering, dynamically updating the displayed countries.The app offers a responsive design for optimal viewing on all devices and includes a dark mode toggle for enhanced accessibility and user comfort ",
     view:"https://countries-api-project-six.vercel.app/",
     source:"https://github.com/mayureshambatwar/Countries-API-project"
+  },
+  {
+    imgsrc: Pro5,
+    title: "Rick and Morty Image Feed",
+    text: "The Image Feed App is a responsive web application built using React.js, TypeScript, and styled with Tailwind CSS, designed to display characters and episodes from the Rick and Morty public API. A left-side navigation bar lists all episodes, allowing users to click and highlight a selected episode, with only one episode highlighted at a time.The app efficiently fetches data using fetch or Axios, ensuring proper handling of API responses and errors. Tailwind CSS provides a mobile-friendly design, adapting seamlessly to various screen sizes. With a clean and intuitive interface, the app enables users to explore characters interactively while demonstrating modern front-end development practices, including React component composition, TypeScript type safety, and a utility-first styling approach.",
+    view:"https://image-feed-gkje.vercel.app/",
+    source:"https://github.com/mayureshambatwar/Image-Feed"
   },
 ];
 
